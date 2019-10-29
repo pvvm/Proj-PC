302e41e51828c5847a2811f5b2e137d6b65ab35e
pvvm <pv.mizuno@hotmail.com>
2019-10-29 14:10:21 -0300

    Randomiza o cardapio

--- projeto.c
+++ projeto.c
@@ -3,18 +3,41 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 int pratos = 0;
 
+sem_t fogao, balcao, forno;
+
 void *cozinha(void *args){
-    
+    int id = *((int *) args);
+    srand(time(0));
+    int cardapio;
+
+    while(pratos > 0){
+        cardapio = rand() % 2;
+        switch(cardapio){
+            case 0:
+                printf("O chef %d preparará uma lasanha.\n", id);
+                sleep(1);
+                break;
+            case 1:
+                printf("O chef %d preparará um cannoli.\n", id);
+                sleep(1);
+                break;
+        }
+        pratos--;
+    }
 }
 
 int main(){
     int n, *id;
-    printf("Quantos chefes serão contratados? ");
+    printf("Quantos chefs serão contratados? ");
     scanf("%d", &n);
     pthread_t chef[n];
+    sem_init(&fogao, 0, 8);
+    sem_init(&balcao,0,6);
+    sem_init(&forno,0,2);
 
     printf("Quantos pratos serão feitos nesse serviço? ");
     scanf("%d", &pratos);
@@ -28,6 +51,10 @@ int main(){
     for(int i = 0; i < n; i++){
         pthread_join(chef[i], NULL);
     }
+
+    sem_destroy(&fogao);
+    sem_destroy(&balcao);
+    sem_destroy(&forno);
 
     return 0;
 }
