e316faf401d51979e83b08f7b8492bd8fbf4f962
pvvm <pv.mizuno@hotmail.com>
2019-10-30 17:42:48 -0300

    Deadlock

--- projeto.c
+++ projeto.c
@@ -5,36 +5,68 @@
 #include <time.h>
 #include <semaphore.h>
 
-int pratos = 0, est_forno;
+int pratos = 0, est_forno = 0, tamanho;
 int num_pratos[5] = {0,0,0,0,0};
 int* sorteio = 0;
+struct fila f;
 
 sem_t sem_fogao, sem_balcao, sem_forno;
-pthread_mutex_t mutex, mutex_forno;
+pthread_mutex_t mutex, mutex_forno, dentro_forno;
+
+struct fila{
+    int id_chef;
+    int saloudoce;
+    int inicio, fim;
+};
+
+void inserir(struct fila *f, int id, int salgadoce){
+    if((f->fim + 1) < tamanho){
+        f->fim++;
+        f->saloudoce = salgadoce;
+        f->id_chef = id;
+    }   
+}
+
+struct fila consumir(struct fila *f){
+    struct fila aux;
+    if(f->inicio < tamanho){
+        aux.id_chef = f->id_chef;
+        aux.saloudoce = f->saloudoce;
+        f->inicio++;
+    }
+    return aux;
+}
 
 void balcao(int id){
     sem_wait(&sem_balcao);
     printf("Chef %d sovando a massa.\n", id);
-    sleep(2);
+    sleep(1);
     sem_post(&sem_balcao);
 }
 
 void fogao(int id){
     sem_wait(&sem_fogao);
     printf("Chef %d cozinhando a massa.\n", id);
-    sleep(3);
+    sleep(1);
     sem_post(&sem_fogao);
 }
 
 void forno(int id, int salgadoce){
-    if(salgadoce == 1 && est_forno == 2)
-        pthread_mutex_lock(&mutex_forno);
-    else if(salgadoce == 2 && est_forno == 1)
-        pthread_mutex_lock(&mutex_forno);
+    pthread_mutex_lock(&mutex_forno);
+    inserir(&f, id, salgadoce);
+    pthread_mutex_unlock(&mutex_forno);
+
+    struct fila proximo;
 
     sem_wait(&sem_forno);
-    est_forno = salgadoce;
-
+    proximo = consumir(&f);
+    if((proximo.saloudoce == 1 && est_forno == 2) || (proximo.saloudoce == 2 && est_forno == 1)){
+        //pthread_mutex_lock(&dentro_forno);
+    }
+    est_forno = proximo.saloudoce;
+    printf("Chef %d assando a massa.\n", proximo.id_chef);
+    sleep(1);
+    //pthread_mutex_unlock(&dentro_forno);
     sem_post(&sem_fogao);
 }
 
@@ -53,7 +85,7 @@ void *cozinha(void *args){
                 printf("O chef %d preparará uma lasanha.\n", id);
                 balcao(id);
                 fogao(id);
-                //forno(id, 1);
+                forno(id, 1);
                 printf("Pedido de lasanha saindo!\n");
                 num_pratos[0]++;
                 break;
@@ -76,12 +108,14 @@ int main(){
 
     pthread_mutex_init(&mutex,0);
     pthread_mutex_init(&mutex_forno,0);
+    pthread_mutex_init(&dentro_forno,0);
     sem_init(&sem_fogao, 0, 8);
     sem_init(&sem_balcao,0,6);
     sem_init(&sem_forno,0,2);
 
     printf("Quantos pratos serão feitos nesse serviço? ");
     scanf("%d", &pratos);
+    tamanho = pratos;
 
     sorteio = (int*) malloc(pratos * sizeof(int));
 
@@ -108,6 +142,7 @@ int main(){
     sem_destroy(&sem_forno);
     pthread_mutex_destroy(&mutex);
     pthread_mutex_destroy(&mutex_forno);
+    pthread_mutex_destroy(&dentro_forno);
 
     free(sorteio);
 
